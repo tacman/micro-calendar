33f0640150d378664e6cfe67c5dd9a2bcc8e9607
Tac Tacelosky <tacman@gmail.com>
2023-10-04 08:35:12 -0400

    add static generator

---
+++ static/assets/@symfony/stimulus-bundle/controllers-abf606f3085d4a46150023eb97dbf33b.js
@@ -0,0 +1,5 @@
+import controller_0 from '../../controllers/hello_controller.js';
+import controller_1 from '../../controllers/api_controller.js';
+export const eagerControllers = {"hello": controller_0, "api": controller_1};
+export const lazyControllers = {};
+export const isApplicationDebug = true;
